f17df6ae47cdd477426853270d04e3a5ba025a84
Rodrigo Ramírez Norambuena <a@rodrigoramirez.com>
2014-11-29 22:41:02 -0300

    sms activeadmin

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -11,14 +11,14 @@ ActiveAdmin.setup do |config|
     admin.authentication_method = :authenticate_admin!
     admin.current_user_method = :current_admin
     admin.logout_link_path = :destroy_admin_session_path
-    admin.root_to = 'calls#index'
+    admin.root_to = 'smss#index'
   end
 
   config.namespace :customers do |customer|
     customer.authentication_method = :authenticate_customer!
     customer.current_user_method = :current_customer
     customer.logout_link_path = :destroy_customer_session_path
-    customer.root_to = 'calls#index'
+    customer.root_to = 'smss#index'
   end
 
 
