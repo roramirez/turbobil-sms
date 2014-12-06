4af3783fc6e9dfcc989bfa096f92ec737d88441c
Rodrigo Ramírez Norambuena <a@rodrigoramirez.com>
2014-12-06 11:48:18 -0300

    add change status on edit campaign (active/inactive)

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -79,6 +79,6 @@ class CampaignsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def campaign_params
-      params.require(:campaign).permit(:init, :name, :text, :list_id)
+      params.require(:campaign).permit(:init, :name, :text, :list_id, :status)
     end
 end
