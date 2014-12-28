c7df9827977d6806d18067960a9a9652a0bea3ac
Rodrigo Ramírez Norambuena <a@rodrigoramirez.com>
2014-12-28 15:26:41 -0300

    set default price customer issue #26

--- db/schema.rb
+++ db/schema.rb
@@ -11,23 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141227164147) do
+ActiveRecord::Schema.define(version: 20141228181241) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "admin", force: true do |t|
     t.text     "name"
-    t.string   "email",                  limit: 320,              null: false
-    t.string   "encrypted_password",                 default: "", null: false
+    t.string   "email",                     limit: 320,              null: false
+    t.string   "encrypted_password",                    default: "", null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                      default: 0,  null: false
+    t.integer  "sign_in_count",                         default: 0,  null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.integer  "default_price_customer_id"
   end
 
   add_index "admin", ["email"], name: "index_admin_on_email", unique: true, using: :btree
