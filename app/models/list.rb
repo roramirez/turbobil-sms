8968f3af671638d19ebb368899469338876c5c27
Rodrigo Ramírez Norambuena <a@rodrigoramirez.com>
2015-08-09 01:57:12 -0400

    add support upload ods files

--- app/models/list.rb
+++ app/models/list.rb
@@ -44,6 +44,7 @@ class List < ActiveRecord::Base
       when ".csv" then Roo::Csv.new(file.path, nil, :ignore)
       when ".xls" then Roo::Excel.new(file.path, nil, :ignore)
       when ".xlsx" then Roo::Excelx.new(file.path, nil, :ignore)
+      when ".ods" then Roo::OpenOffice.new(file.path, nil, :ignore)
       else raise "Unknown file type: #{file.original_filename}"
     end
   end
